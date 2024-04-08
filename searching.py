a2952e3bbdda8d54a243a100c453c44e85f438ef
Eva Vorlova <eva.vorlova13@seznam.cz>
2024-04-08 13:41:58 +0200

    searching3

--- searching.py
+++ searching.py
@@ -51,6 +51,19 @@ def pattern_search(sequence, target):
             positions.add(i)
     return positions
 
+def binary_search(numbers, target):
+    left, right = 0, len(numbers) - 1
+
+    while left <= right:
+        mid = (left + right) //2
+        if numbers[mid] == target:
+                return mid
+        elif numbers[mid] < target:
+                left = mid + 1
+        else:
+                right = mid -1
+    return None
+
 
 if __name__ == '__main__':
     main()
